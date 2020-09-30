c4a707b73c7dbea47f1e887346f1aa98ba1f206f
rubber-duckie-au <rubberduckie.au@gmail.com>
2020-10-01 09:07:40 +1000

    MN, Coonections, Blocks Widget

--- routes/index.js
+++ routes/index.js
@@ -297,6 +297,7 @@ router.get('/ext/summary', function(req, res) {
     lib.get_hashrate(function(hashrate) {
       lib.get_connectioncount(function(connections){
         lib.get_blockcount(function(blockcount) {
+          lib.get_masternodecount(function(masternodecount){
           db.get_stats(settings.coin, function (stats) {
             if (hashrate == 'There was an error. Check your console.') {
               hashrate = 0;
@@ -310,6 +311,7 @@ router.get('/ext/summary', function(req, res) {
               connections: connections,
               blockcount: blockcount
             }]});
+          });
           });
         });
       });
